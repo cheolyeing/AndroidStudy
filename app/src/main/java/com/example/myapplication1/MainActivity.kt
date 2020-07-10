eba2afb30bd5b44aef29f7a49252e37c785f02e4
cheolyeing <cheolyeing@gmail.com>
2020-07-10 12:32:45 +0900

    Study Widget button, editText, textView, imageView

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.myapplication1
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var button: Button?
+        button_new.setOnClickListener {
+            var str = editText_new.text
+            println(str)
+            textView_new.text = str
+            imageView_new.setImageResource(R.drawable.picture2)
+        }
     }
 }
